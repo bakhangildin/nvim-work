3fa7bdb3afdb7c86189aa0272d8a94a53be0502a
Булат Хангильдин <hangildin@bo-energo.ru>
2024-08-21 13:00:18 +0300

    Change Gitsigns signs

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -8,7 +8,20 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup {
+        signs = {
+          add = { text = "+" },
+          change = { text = "~" },
+          delete = { text = "-" },
+          topdelete = { text = "‾" },
+        },
+        signs_staged = {
+          add = { text = "+" },
+          change = { text = "~" },
+          delete = { text = "-" },
+          topdelete = { text = "-" },
+        },
+      }
     end,
   },
 }
