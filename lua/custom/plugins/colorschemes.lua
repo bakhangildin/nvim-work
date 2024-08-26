8ed745733524ea0f097b4f4e15848693ab3e48e4
Булат Хангильдин <hangildin@bo-energo.ru>
2024-08-26 18:37:05 +0300

    colors

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -5,10 +5,19 @@ return {
       vim.cmd.colorscheme "gruvbuddy"
 
       local bg = "#222222"
-      vim.api.nvim_set_hl(0, "Normal", { bg = bg })
+      vim.api.nvim_set_hl(0, "Normal", { bg = bg, fg = "#dddddd" })
       vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#303030" })
       vim.api.nvim_set_hl(0, "LineNr", { bg = bg })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#181818" })
+      vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "DiagnosticUnnecessary" })
     end,
   },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    config = function()
+      --vim.cmd.colorscheme "tokyonight-night"
+      --vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "DiagnosticUnnecessary" })
+    end
+  }
 }
